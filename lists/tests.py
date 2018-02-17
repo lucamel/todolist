099db01947bc9139576477e79e7bac44f714fe87
lucamelgrati <lucamel11@gmail.com>
2018-02-17 16:11:55 +0100

    add Lists

---
+++ lists/tests.py
@@ -0,0 +1,20 @@
+from django.http import HttpRequest
+from django.test import TestCase
+from django.urls import resolve
+
+from lists.views import home_page
+
+class HomePageTest(TestCase):
+
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
+
+    def test_home_page_is_about_todo_lists(self):
+        request = HttpRequest()
+
+        response = home_page(request)
+
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>To-Do Lists</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
